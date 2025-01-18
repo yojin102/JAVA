c33e78548b40dfa101107c8f616bc308bd54edbe
yojin102 <121215616+yojin102@users.noreply.github.com>
2025-01-18 12:35:57 +0900

    Add files via upload

---
+++ untitled2/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;   // 입력 라이브러리 클래스
+public class Main {
+    public static void main(String[] args) {
+
+        // 문자열 출력
+        System.out.print("Hello world!");   // 출력 후 줄 바꿈 xx
+        System.out.println("Hello world!"); // 출력 후 줄 바꿈
+
+        Scanner stdin = new Scanner(System.in); // 키보드 입력을 위한 Scanner 객체 생성
+        String s = stdin.next();    // 사용자의 입력을 문자열로 받음
+        // int i = stdin.nextInt();
+        // double d = stdin.nextDouble();
+
+        // 주석문
+        /* 여러 줄의 문장 주석처리 */
+        
+    }
+}
